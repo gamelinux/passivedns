74d5a0efe8c07ed6487c373651826b813cce812f
Dave Crawford <github@pingtrip.com>
2011-11-11 22:44:39 +0000

    fixed: compilation on BSD was broken

--- src/passivedns.h
+++ src/passivedns.h
@@ -74,6 +74,10 @@
 #define ERROR       1
 #define STDBUF      1024
 
+#ifdef __FreeBSD__
+#define s6_addr32   __u6_addr.__u6_addr32
+#endif /* __FreeBSD__ */
+
 /*  D A T A  S T R U C T U R E S  *********************************************/
 
 /* 
@@ -528,3 +532,23 @@ typedef struct _globalconfig {
 #define vlog(fmt, ...) do { ; } while(0)
 #endif
 
+#ifdef __FreeBSD__
+const char *_res_opcodes[] = {
+        "QUERY",
+        "IQUERY",
+        "CQUERYM",
+        "CQUERYU",      /* experimental */
+        "NOTIFY",       /* experimental */
+        "UPDATE",
+        "6",
+        "7",
+        "8",
+        "UPDATEA",
+        "UPDATED",
+        "UPDATEDA",
+        "UPDATEM",
+        "UPDATEMA",
+        "ZONEINIT",
+        "ZONEREF",
+};
+#endif /* __FreeBSD__ */
