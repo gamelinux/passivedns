700613004e2a4a151b1d25b9d034a3b9afbd37be
gamelinux <edwardfjellskaal@gmail.com>
2013-02-01 16:29:37 +0000

    Bumped version to 1.1.1

--- src/passivedns.h
+++ src/passivedns.h
@@ -22,7 +22,7 @@
 /*  I N C L U D E S  **********************************************************/
 
 /*  D E F I N E S  ************************************************************/
-#define VERSION                       "1.0"
+#define VERSION                       "1.1.1"
 #define TIMEOUT                       60
 #define BUCKET_SIZE                   65537
 #define SNAPLENGTH                    1600
