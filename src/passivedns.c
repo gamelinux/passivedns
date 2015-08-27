9f0d25ffa21eb706bce13b51c5fa4a0c3c1b7a06
Edward Fjellskål <edwardfjellskaal@gmail.com>
2015-08-27 21:18:14 +0200

    fixed minor typo

--- src/passivedns.c
+++ src/passivedns.c
@@ -1101,7 +1101,7 @@ void usage()
     olog("   4:A      6:AAAA  C:CNAME  D:DNAME  N:NAPTR  O:SOA  L:LOC   F:SPF   I:HINFO\n");
     olog("   P:PTR    R:RP    S:SRV    T:TXT    M:MX     n:NS   d:DNSEC H:SSHFP\n");
     olog("   L also enables GPOS\n");
-    olog("   d enables DS, DNSKEY, NSEC, NSEC3. NSEC3PARAM, RRSIG\n");
+    olog("   d enables DS, DNSKEY, NSEC, NSEC3, NSEC3PARAM, RRSIG\n");
 
     olog("\n");
     olog(" * For Server Return Code (SRC) Errors:\n");
