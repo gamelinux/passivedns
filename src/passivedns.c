9a02862037c217dc9cf54569a8bc37bb13fcce11
Edward Fjellskål <edwardfjellskaal@gmail.com>
2015-08-27 20:28:24 +0200

    added correct parameter info for HINFO

--- src/passivedns.c
+++ src/passivedns.c
@@ -1098,7 +1098,7 @@ void usage()
     olog(" FLAGS:\n");
     olog("\n");
     olog(" * For Record Types:\n");
-    olog("   4:A      6:AAAA  C:CNAME  D:DNAME  N:NAPTR  O:SOA  L:LOC   F:SPF   H:HINFO\n");
+    olog("   4:A      6:AAAA  C:CNAME  D:DNAME  N:NAPTR  O:SOA  L:LOC   F:SPF   I:HINFO\n");
     olog("   P:PTR    R:RP    S:SRV    T:TXT    M:MX     n:NS   d:DNSEC H:SSHFP\n");
     olog("   L also enables GPOS\n");
     olog("   d enables DS, DNSKEY, NSEC, NSEC3. NSEC3PARAM, RRSIG\n");
