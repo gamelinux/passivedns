ff10c29af607200e4e6b48a3d94fe3ae03d1dada
Maxtors <moe.andreas@gmail.com>
2016-01-09 18:39:23 +0000

    Added redis config fields to global config

--- src/passivedns.h
+++ src/passivedns.h
@@ -494,6 +494,10 @@ typedef struct _globalconfig {
     uint8_t             print_updates;     /* Prints updates */
     uint8_t             output_log;        /* Log to log file */
     uint8_t             output_log_nxd;    /* Log NXDOMAIN to log file */
+#ifdef HAVE_LIBHIREDIS
+    uint16_t            redis_port;        /* Redis Port */
+    char                *server;           /* Redis Server */
+#endif /* HAVE_LIBHIREDIS */
     uint8_t             output_syslog;     /* Log to syslog */
     uint8_t             output_syslog_nxd; /* Log NXDOMAIN to syslog */
 #ifdef HAVE_JSON
